64d3b2ed17defa502ddf6abd39a3e3637592711e
wlq <wlq314@qq.com>
2016-05-30 17:19:09 +0800

    修改数据库配置项

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
 	'password' => 'w314314',
 	'database' => 'app_kuangshi',
