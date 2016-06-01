fb75807b37a58abcaa4e21de622f52e6258797c5
wlq <wlq314@qq.com>
2016-06-01 23:21:49 +0800

    seaslog test

--- application/controllers/Slog.php
+++ application/controllers/Slog.php
@@ -1,25 +1,38 @@
 <?php
 /**
- * Slog.php
+ * Log.php
  * User: wlq314@qq.com
  * Date: 16/6/1 Time: 09:52
  */
-class Slog extends CI_Controller{
+class Log extends CI_Controller{
     
     private $_basepath;
+    private $_logger;
     
     public function __construct()
     {
         parent::__construct();
-        $this->_basepath = '/data/logs/seaslog/';
+        $this->_basepath = '/data/logs/';
+        $this->_logger = 'wechat-ci';
         Seaslog::setbasepath($this->_basepath);
+        SeasLog::setLogger($this->_logger);
     }
 
     public function setbasepath($path){
         return Seaslog::setBasePath($path);
     }
 
+    /**
+     * DEBUG方法
+     * @param $message
+     * @param array $content
+     * @param string $module
+     */
+    public function debug($message, array $content = [], $module = ''){
+        return SeasLog::debug($message, $content, $module);
+    }
+
     public function log($content){
-        return Seaslog::debug($content);
+        return Seaslog::info($content);
     }
 }
