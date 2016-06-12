515e8b015a6230eec33e10d7268be5862509d6b7
wlq <wlq314@qq.com>
2016-06-12 17:23:31 +0800

    wechat测试

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -9,6 +9,7 @@ class Wechat extends MY_Controller{
     
     public function index(){
         $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        var_dump($postArr);
         SeasLog::info("访问信息", $postArr);
         $this->checkweixin();
     }
