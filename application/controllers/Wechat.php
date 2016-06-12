36e6d8b1c5e821c308ede964d49d636224a38437
wlq <wlq314@qq.com>
2016-06-12 17:07:01 +0800

    wechat测试

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -8,18 +8,18 @@
 class Wechat extends MY_Controller{
     
     public function index(){
-        print_r($this->config);
-        print_r($this->config->item('wechat'));
-
+        $this->checkweixin();
     }
 
     //验证微信TOKEN
-    private function checkweixin($token){
+    private function checkweixin(){
+        $wechatInfo = $this->config->item('wechat');
+        
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce     = $_GET["nonce"];
 
-        $tmpArr = array($token, $timestamp, $nonce);
+        $tmpArr = array($wechatInfo['token'], $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
@@ -32,7 +32,7 @@ class Wechat extends MY_Controller{
             }
             return true;
         }else{
-            return false;
+            die('请通过微信平台访问');
         }
     }
 }
