85c060d68d31febc18b173af32ed6a5ac8326c57
wlq <wlq314@qq.com>
2016-06-13 10:56:55 +0800

    test debug log

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -10,6 +10,7 @@ class Wechat extends MY_Controller{
     public function index(){
         $postArr = $GLOBALS['HTTP_RAW_POST_DATA'] ? $GLOBALS['HTTP_RAW_POST_DATA'] : ['空POST'];
         SeasLog::debug("请求POST信息:".var_export($postArr, true));
+        SeasLog::debug("请求GET信息:".var_export($_GET, true));
         $this->checkweixin();
     }
 
